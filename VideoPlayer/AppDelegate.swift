358ad28d870b3b21652f92c6bcb940103eebccf6
Chi-AnTai <andy.chian.tai@gmail.com>
2017-09-06 17:52:45 +0800

    fix swiftlint

--- VideoPlayer/AppDelegate.swift
+++ VideoPlayer/AppDelegate.swift
@@ -11,62 +11,48 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+// swiftlint:disable line_length
     var window: UIWindow?
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        
+
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-       
+
         self.saveContext()
     }
-
-    // MARK: - Core Data stack
-
     lazy var persistentContainer: NSPersistentContainer = {
                let container = NSPersistentContainer(name: "VideoPlayer")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        container.loadPersistentStores(completionHandler: { (_, error) in
             if let error = error as NSError? {
                              fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
         return container
     }()
-
-    // MARK: - Core Data Saving support
-
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
             do {
                 try context.save()
             } catch {
-                
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
     }
-
 }
-
--- VideoPlayer/VideoPlayerViewController.swift
+++ VideoPlayer/VideoPlayerViewController.swift
@@ -1,20 +1,15 @@
-
-
 import UIKit
 import AVKit
 import AVFoundation
 
-
-
-class VideoPlayerViewController: UIViewController,UITextFieldDelegate {
-    
-   
+class VideoPlayerViewController: UIViewController, UITextFieldDelegate {
+// swiftlint:disable line_length
     var videoPlayItem: AVPlayerItem?
     let avPlayer = AVPlayer()
     let avPlayerLayer = AVPlayerLayer()
     var playButton: UIButton?
     var muteButton: UIButton?
-    
+
     override func viewDidLoad() {
         self.view.backgroundColor = UIColor.black
         setPlayButton()
@@ -23,95 +18,62 @@ class VideoPlayerViewController: UIViewController,UITextFieldDelegate {
 
         avPlayer.addObserver(self, forKeyPath: "volume", options: .new, context: nil)
         avPlayer.addObserver(self, forKeyPath: "rate", options: .new, context: nil)
-        
-        
             }
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        
         if keyPath == "rate" {
             if let rate = change?[NSKeyValueChangeKey.newKey] as? Float {
                 if rate != 0.0 {
-                    playButton?.setTitle("pause", for: .normal)
-                
-                }
-                else {
+                playButton?.setTitle("pause", for: .normal)
+                } else {
                     playButton?.setTitle("play", for: .normal)
-
                 }
             }
         }
         if keyPath == "volume" {
-            
-            
             if let isMuted = change?[NSKeyValueChangeKey.newKey] as? Float {
-                if isMuted == 0.0  {
+                if isMuted == 0.0 {
                     muteButton?.setTitle("unMute", for: .normal)
-                    
-                }
-                else {
+                } else {
                     muteButton?.setTitle("mute", for: .normal)
-                    
                 }
             }
-
-        
         }
-        
-        
-        
     }
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-       
     }
     func setPlayButton() {
      playButton = UIButton(frame: CGRect(x: 0.0, y: self.view.frame.size.height-60.0, width: 60.0, height: 60.0))
         playButton?.setTitle("play", for: .normal)
         self.view.addSubview(playButton!)
         playButton?.addTarget(self, action: #selector(playButtonAction), for: .touchUpInside)
-        
-    
     }
     func playButtonAction(sender: UIButton) {
-        if let title = sender.titleLabel?.text  {
+        if let title = sender.titleLabel?.text {
             if title == "pause" {
             avPlayer.pause()
-            }
-            else {
+            } else {
             avPlayer.play()
             }
-            
-            
         }
-       
-
     }
-    
     func setMuteButton() {
         muteButton = UIButton(frame: CGRect(x: self.view.frame.size.width-80.0, y: self.view.frame.size.height-60.0, width: 80.0, height: 60.0))
         muteButton?.setTitle("mute", for: .normal)
         muteButton?.addTarget(self, action: #selector(muteButtonAction), for: .touchUpInside)
-        
         self.view.addSubview(muteButton!)
     }
-    
+
     func muteButtonAction(sender: UIButton) {
-        if let title = sender.titleLabel?.text  {
+        if let title = sender.titleLabel?.text {
             if title == "mute" {
-                
+
                 avPlayer.volume = 0
-            }
-            else {
+            } else {
                 avPlayer.volume = 3
             }
-            
-            
         }
-
     }
-    
     func setURLTextField() {
         let urlTextField = UITextField(frame: CGRect(x: 8.0, y: 27.0, width: 359.0, height: 30.0))
         urlTextField.placeholder = "Enter URL of video"
@@ -120,13 +82,11 @@ class VideoPlayerViewController: UIViewController,UITextFieldDelegate {
         urlTextField.delegate = self
         urlTextField.addTarget(self, action: #selector(textFieldDidEnd(textField:)), for: .editingDidEnd)
         self.view.addSubview(urlTextField)
-        
     }
     func textFieldDidEnd(textField: UITextField) {
         if let url = textField.text, let videoURL = URL(string: url) {
         setAVPlayer(url: videoURL)
         }
-        
     }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true)
@@ -139,9 +99,5 @@ class VideoPlayerViewController: UIViewController,UITextFieldDelegate {
         avPlayerLayer.frame = self.view.bounds
         self.view.layer.addSublayer(avPlayerLayer)
         avPlayer.play()
-    
     }
-
-
 }
-
