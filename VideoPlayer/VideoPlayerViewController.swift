32774a0e607d9de6e42c82f0e38434be3d419062
Chi-AnTai <andy.chian.tai@gmail.com>
2017-09-06 14:27:45 +0800

    add avplayer and it can successful load video from url

--- VideoPlayer/VideoPlayerViewController.swift
+++ VideoPlayer/VideoPlayerViewController.swift
@@ -1,11 +1,18 @@
 
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class VideoPlayerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let videoURL = URL(string: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8")
+        let avplayer = AVPlayer(url: videoURL!)
+        let avplayerLayer = AVPlayerLayer(player: avplayer)
+        avplayerLayer.frame = self.view.bounds
+        self.view.layer.addSublayer(avplayerLayer)
         
     }
 
